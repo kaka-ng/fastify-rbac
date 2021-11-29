df294af6f9a2b9520897238b6e8e584978cb3b44
KaKa <kaka@kakawebsitedemo.com>
2021-11-29 14:46:14 +0800

    build: add external for rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,13 +6,15 @@ export default [
     output: [
       { dir: 'lib', format: 'cjs', sourcemap: true },
     ],
-    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib', include: ['lib/**/*'] })]
+    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib', include: ['lib/**/*'] })],
+    external: ['@kakang/validator', 'fastify-plugin']
   },
   {
     input: 'lib/index.ts',
     output: [
       { dir: 'lib/mjs', format: 'esm', sourcemap: true },
     ],
-    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib/mjs', include: ['lib/**/*'] })]
+    plugins: [TypeScript({ module: 'ESNext', outDir: 'lib/mjs', include: ['lib/**/*'] })],
+    external: ['@kakang/validator', 'fastify-plugin']
   },
 ]
